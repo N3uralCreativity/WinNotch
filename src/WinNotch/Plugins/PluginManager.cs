72b6f15ca3da41858f54f102468f4fd29cb4721d
N3uralCreativity <leo.peron33@gmail.com>
2026-04-20 01:13:35 +0200

    v0.3.3: Plugin UI injection + manager foreground fix + improved uninstaller
    - Plugins now display UI in the notch (closed + open panels)
    - Plugin state change notifications (Open/Peek/Close)
    - Plugin Manager window opens in foreground
    - Uninstaller kills running process and offers to delete all data
    - Bumped version to 0.3.3

--- src/WinNotch/Plugins/PluginManager.cs
+++ src/WinNotch/Plugins/PluginManager.cs
@@ -256,7 +256,7 @@ public class PluginManager : IDisposable
     {
         try
         {
-            var currentVersion = new Version("0.3.2"); // WinNotch version
+            var currentVersion = new Version("0.3.3"); // WinNotch version
             var required = new Version(requiredVersion);
             return currentVersion >= required;
         }
